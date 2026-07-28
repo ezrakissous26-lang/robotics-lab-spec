f1c640d7ee7ec5721926f7fa00d71815d485c488
Ezra <ezrakissous26@gmail.com>
2026-07-29 01:45:17 +0300

    Create repo functions for students and use students routes

--- repos/students-repo.js
+++ repos/students-repo.js
@@ -4,10 +4,14 @@ import { ObjectId } from 'mongodb'
 const db = client.db('students')
 const students = db.collection('students')
 
-export async function createStudents(body) {
+export async function createStudent(body) {
+    const student = {
+        ...body,
+        labSessionsIds: []
+    }
     try {
-        const result = await students.insertOne(body)
-        console.log(result)
+        const result = await students.insertOne(student)
+        return result
     } catch (e) {
         console.error('Error :', e.message)
     }
@@ -17,10 +21,37 @@ export async function getById(id) {
     try {
         const result = await students.find({ "_id": new ObjectId(id)}).toArray()
         console.log(result)
+        return result
     } catch (e) {
         console.error('Error :', e.message)
     }
 }
 
+export async function getAll() {
+    try {
+        const result = await students.find().toArray()
+        console.log(result)
+    } catch (e) {
+        console.error('Error :', e.message)
+    }
+}
+
+export async function updateLabSessionIds(studentId, sessionId) {
+    try {
+        const result = await students.updateOne(
+            {_id: new ObjectId(studentId)},
+            {$push: { labSessionsIds: sessionId}})
+            console.log(result)
+    } catch (e) {
+        console.error('Error :', e.message)
+    }
+}
+
+
 //await createStudents()
 // await getById('6a68f118256acaffc50518d2')
+
+
+// await updateLabSessionIds('6a68f13a1fe080baabf8264e', '90e89f5c-1cdb-4865-8074-ce68234b7d9e')
+
+// await getAll()
--- routes/students-routes.js
+++ routes/students-routes.js
@@ -1,13 +1,21 @@
 import express from 'express'
+import { createStudent, getById } from '../repos/students-repo.js'
 
 const router = express.Router()
 
-router.post('/', (req, res) => {
-    res.status(201).json({message: 'router post'})
+router.post('/', async (req, res) => {
+    const student = req.body
+    const result = await createStudent(student)
+    console.log(result)
+    res.status(201).json({id: result.insertedId})
 })
 
-router.get('/:usersId', (req, res) => {
-    res.status(201).json({message: 'router get'})
+router.get('/:usersId', async (req, res) => {
+    const objectId = req.params
+    console.log(objectId)
+    const result = await getById(objectId.usersId)
+    console.log(result)
+    res.status(200).json(result)
 })
 
 export default router
